7e39c68e5847ba7d37320d0ec8df3ee1d63abd30
Jessica Schultz <jessicamschultz@icloud.com>
2019-02-01 12:34:45 -0500

    Update Model.elm

--- Examples/Accordion/src/Model.elm
+++ Examples/Accordion/src/Model.elm
@@ -13,11 +13,16 @@ import Bootstrap.Navbar as Navbar
 import Bootstrap.Accordion as Accordion
 import Bootstrap.Card.Block as Block
 
+--ADD CAROUSEL imports to main.elm, model.elm, and the page using carousel (ex. Home.elm)
+import Bootstrap.Carousel as Carousel
+import Bootstrap.Carousel.Slide as Slide
+
 type alias Model =
     { navKey : Navigation.Key
     , page : Page
     , navState : Navbar.State
     , accordionState : Accordion.State --ADD ACCORDION
+    , carouselState : Carousel.State -- ADD CAROUSEL
     }
 
 type Page
@@ -31,3 +36,4 @@ type Msg
     | NavMsg Navbar.State
     | NoOp
     | AccordionMsg Accordion.State --ADD ACCORDION
+    | CarouselMsg Carousel.Msg --ADD CAROUSEL
